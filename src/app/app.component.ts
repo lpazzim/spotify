f7c3d5f9e7d3d42bea4b0ad16c9bc1fd88a8b138
Lucas Pazzim <lpazzim@gmail.com>
2021-05-14 16:33:06 -0300

    initial redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  protected router: Router;
   title = 'spotify';
 
+  constructor(
+    protected injector: Injector,
+  ){
+    this.router = this.injector.get(Router);
+  }
 
 
+  ngOnInit() {
+    this.router.navigate(['/login']);
+  }
 }
